84818d8c3776944959739070b169d9935bbb179a
NeuroSys <notrealemail@example.com>
2022-12-07 21:51:09 +0200

    Add script to modify the key repeat interval

---
+++ key-repeat.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Changes the rate at witch keys will repeat when holding a key pressed
+# - first number is in milliseconds and represents how long until auto-repeat starts
+# - second number is how many times per second to repeat
+xset r rate 200 30
+
