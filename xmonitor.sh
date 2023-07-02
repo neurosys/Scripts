9f72105a1c57d48b18446d2fee23025a3b16695b
NeuroSys <you@example.com>
2023-07-02 22:42:05 +0300

    xmonitor for palpatine

--- xmonitor.sh
+++ xmonitor.sh
@@ -2,25 +2,25 @@
 
 if [[ "$1" == "left" ]]
 then
-    xrandr --output DisplayPort-1 --auto --set TearFree on --output DisplayPort-2 --off
+    xrandr --output DisplayPort-0 --auto --set TearFree on --output DisplayPort-1 --off
 
 fi
 
 if [[ "$1" == "right" ]]
 then
-    xrandr --output DisplayPort-1 --off --output DisplayPort-2 --auto --set TearFree on
+    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --auto --set TearFree on
 
 fi
 
 if [[ "$1" == "all" ]]
 then
-    xrandr --output DisplayPort-1 --auto --set TearFree on --left-of DisplayPort-2 --output DisplayPort-2 --auto --set TearFree on
+    xrandr --output DisplayPort-0 --auto --set TearFree on --left-of DisplayPort-1 --output DisplayPort-1 --auto --set TearFree on
 
 fi
 
 if [[ "$1" == "none" ]]
 then
-    xrandr --output DisplayPort-1 --off --output DisplayPort-2 --off
+    xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off
 fi
 
 cd ~/.myconfig/xorg/
