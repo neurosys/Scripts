bd5a08d5e8de933e9efc934aecae8f3a26fc62e2
NeuroSys <not.my.email@gotohell.com>
2025-03-31 18:24:25 +0300

    Improved the whatsapp script that converts videos

--- convertForWhatsapp.sh
+++ convertForWhatsapp.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-ffmpeg -i to_view/Pilot.mp4 -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p sendable_video.mp4
+input=$1
+output=$2
+
+if [[ "$input" == "" || "$output" == "" ]]
+then
+    echo "$0 <input_file> <output_file>"
+    echo -e "\toutput file it's recommented to be an .mp4"
+    echo ""
+    exit 0
+fi
+
+ffmpeg -i "$input" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p "$2" sendable_video.mp4
