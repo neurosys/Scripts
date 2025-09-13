5ed793a6c3c7972dddadf1d7c62f76af8d98e989
NeuroSys <not.my.email@gotohell.com>
2025-09-13 16:33:23 +0300

    key-repeat.sh change try a more smoother configuration

--- key-repeat.sh
+++ key-repeat.sh
@@ -3,5 +3,5 @@
 # Changes the rate at witch keys will repeat when holding a key pressed
 # - first number is in milliseconds and represents how long until auto-repeat starts
 # - second number is how many times per second to repeat
-xset r rate 200 30
+xset r rate 180 35
 
